6e46b79c52b5765fc7a418d9f16d37c46c7416f5
lwscroggins <larry.scroggins@gmail.com>
2014-08-06 23:14:04 -0700

    updates to make script more readable

--- MMandM.js
+++ MMandM.js
@@ -3,44 +3,46 @@ var numArray = [];
 
 
 //setup
-process.argv.forEach(function(val, index, array) {
-  if(index >= 2) {
-    numArray.push(Number(val));  
-  }
-});
+  process.argv.forEach(function(val, index, array) {
+    if(index >= 2) {
+      numArray.push(Number(val));  
+    }
+  });
 
-numArray.sort(function(a, b) {
-  return a-b;
-});
+  numArray.sort(function(a, b) {
+    return a-b;
+  });
 
-sum = numArray.reduce(function(a,b){
-  return a + b;
-});
+  sum = numArray.reduce(function(a,b){
+    return a + b;
+  });
 
 //mean
-mean = sum / numArray.length;
-console.log("The mean is: " + mean);
+  mean = sum / numArray.length;
+
 
 //median
-numArray.length % 2 !== 0 ? j = (numArray.length -1)/2 : j = numArray.length/2;
-median = numArray[j];
-console.log("The median is: " + median);
+  numArray.length % 2 !== 0 ? j = (numArray.length -1)/2 : j = numArray.length/2;
+  median = numArray[j];
 
 //Mode
-var counts = [];
+  var counts = [];
 
-for (var i = 0; i < numArray.length; i++) {
-  counts[numArray[i]] = (counts[numArray[i]] ? counts[numArray[i]] : 0) + 1;
-}
-
-var mode = 0;
-var modeNum = 0;
-
-counts.forEach(function(value, index){
-  if (value >= modeNum) {
-    mode = index;
-    modeNum = value;
+  for (var i = 0; i < numArray.length; i++) {
+    counts[numArray[i]] = (counts[numArray[i]] ? counts[numArray[i]] : 0) + 1;
   }
-});
 
-console.log("The mode is: " + mode);
+  var mode = 0;
+  var modeNum = 0;
+
+  counts.forEach(function(value, index){
+    if (value >= modeNum) {
+      mode = index;
+      modeNum = value;
+    }
+  });
+
+//output
+  console.log("The mean is: " + mean);
+  console.log("The median is: " + median);
+  console.log("The mode is: " + mode);
